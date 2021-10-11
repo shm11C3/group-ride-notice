7469d0040dd7b8c0f4c2dc7560c213dfaea22e5c
shm11C3 <s.murakami.tech@gmail.com>
2021-10-12 01:18:19 +0900

    アカウント情報の表示を調整

--- backend/resources/views/auth/dashboard.blade.php
+++ backend/resources/views/auth/dashboard.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.layout')
 @section('title','ダッシュボード')
 @section('content')
-<h3>プロフィール</h3>
+<h3>登録情報</h3>
 <div class="profile mt-5">
     <div class="row">
         <div class="col-8">
             <div class="row p-3 border-bottom">
-                <span class="col-3 text-right profile-label">名前</span><span class="col-3 font-weight-bold">{{ $user[0]->name }}</span>
+                <span class="col-3 text-right profile-label">名前</span><span class="col-9 font-weight-bold">{{ $user[0]->name }}</span>
             </div>
             <div class="row p-3 border-bottom">
-                <span class="col-3 text-right profile-label">メールアドレス</span><span class="col-3 font-weight-bold">{{ $user[0]->email }}</span>
+                <span class="col-3 text-right profile-label">メールアドレス</span><span class="col-9 font-weight-bold">{{ $user[0]->email }}</span>
             </div>
             <div class="row p-3 border-bottom">
-                <span class="col-3 text-right profile-label">都道府県</span><span class="col-3 font-weight-bold">{{ $prefecture }}</span>
+                <span class="col-3 text-right profile-label">都道府県</span><span class="col-9 font-weight-bold">{{ $prefecture }}</span>
             </div>    
         </div>
         <div class="col-4">

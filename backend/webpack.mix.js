d47a24489d5e1395c7eb34803bafb6ec97f03aef
shm11C3 <s.murakami.tech@gmail.com>
2021-10-13 17:27:09 +0900

    本番環境の構築

--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -14,14 +14,14 @@ const mix = require('laravel-mix');
 mix.sourceMaps().js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps();
 
 mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css', {
-      sassOptions: {
-        includePaths: [
-          'node_modules',
-          'node_modules/bootstrap-honoka/scss'
-        ]
-      }
-  });
+  //.sass('resources/sass/app.scss', 'public/css', {
+  //    sassOptions: {
+  //      includePaths: [
+  //        'node_modules',
+  //        'node_modules/bootstrap-honoka/scss'
+  //      ]
+  //    }
+  //});
 
 
 mix.js('resources/js/home.js', 'public/js/home.js').vue();

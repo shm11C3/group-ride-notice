fec8daecab1f5a78c6df8cf2485b9d435727a0f1
shm@desktop-ubuntu <s.murakami.tech@gmail.com>
2021-10-28 12:06:06 +0900

    developブランチの前回のコミットを修正

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\DB;
-//use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {

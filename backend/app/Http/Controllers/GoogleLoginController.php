2e79851bdb5fd78bab5d710f268f325fb08e6d82
shm@desktop-ubuntu <s.murakami.tech@gmail.com>
2021-11-29 02:43:32 +0900

    Google認証処理を実装

--- backend/app/Http/Controllers/GoogleLoginController.php
+++ backend/app/Http/Controllers/GoogleLoginController.php
@@ -12,51 +12,63 @@ use Laravel\Socialite\Facades\Socialite;
 
 class GoogleLoginController extends Controller
 {
+    private $googleAuthError = ['googleAuthError' => '予期せぬエラーが発生しました。'];
+
+    /**
+     * Google認証画面にリダイレクト
+     *
+     * @param void
+     * @return redirect
+     */
     public function getGoogleAuth()
     {
-        return Socialite::driver('google')
-            ->redirect();
+        return Socialite::driver('google')->redirect();
     }
 
     /**
-     * Google認証成功時の処理
+     * ユーザがログインしていない場合の処理
      *
-     * @param Request
-     * @return redirect
+     * @param object $googleUser
+     * @return string $auth_uuid
      */
-    public function authGoogleCallback(Request $request)
+    private function loginUser(object $googleUser)
     {
-        $googleUser = Socialite::driver('google')->user();
+        $googleUserData = DB::table('google_users')->where('google_id', $googleUser->id)->get('user_uuid');
 
-        $auth_uuid = Auth::user()->uuid ?? 0;
+        if(!isset($googleUserData[0])){
+            // google連携アカウントに一致するBipokeleアカウントが登録されていない場合
+            $auth_uuid = Str::uuid();
+            $user_id = $this->createUser($googleUser, $auth_uuid);
 
-        // 認証済みでない場合にユーザを新規作成する
-        if(!$auth_uuid){
-            $auth_uuid = $this->createUser($googleUser);
+        }else{
 
-            // [todo] uuidを用いた認証処理を実装
+            $auth_user = User::where('uuid', $googleUserData[0]->user_uuid)->get(['id', 'uuid']);
+
+            if(isset($auth_user[0])){
+                $auth_uuid = $auth_user[0]->uuid;
+                $user_id = $auth_user[0]->id;
+
+            }else{
+                // Googleアカウントと連携済みだが、アカウントが存在しない場合
+                $auth_uuid = $googleUserData[0]->user_uuid;
+                $user_id = $this->createUser($googleUser, $auth_uuid);
+            }
         }
+        Auth::loginUsingId($user_id, $remember = true);
 
-        GoogleUser::firstOrCreate([
-            'google_id' => $googleUser->id,
-        ], [
-            'user_uuid' => $auth_uuid
-        ]);
-
-        // [todo] 本登録処理を実装
-        return redirect()->route('showRegisterOAuthUser');
+        return $auth_uuid;
     }
-
 
     /**
      * ユーザの新規作成
      *
      * @param object $googleUser
+     * @param string $auth_uuid
+     *
      * @return string $auth_uuid
      */
-    private function createUser(object $googleUser)
+    private function createUser(object $googleUser, string $auth_uuid)
     {
-        $auth_uuid = Str::uuid();
         DB::beginTransaction();
         try{
 
@@ -68,8 +80,8 @@ class GoogleLoginController extends Controller
                         'user_profile_img_path' => $googleUser->avatar
                     ]);
 
-                DB::table('users')
-                    ->insert([
+                $user_id = DB::table('users')
+                    ->insertGetId([
                         'uuid' => $auth_uuid,
                         'name' => $googleUser->nickname ?? $googleUser->name,
                         'prefecture_code' => 0,
@@ -83,7 +95,37 @@ class GoogleLoginController extends Controller
             abort(500);
         }
 
-        return $auth_uuid;
+        return $user_id;
+    }
+
+    /**
+     * Google認証成功時の処理
+     *
+     * ログイン済みのユーザはgoogle連携のみ行いダッシュボードへ戻す
+     * ログインしていないが、一度同じgoogleアカウントで認証をしているユーザはそのアカウントでログインしてダッシュボードへ戻す
+     * Bipokeleログインをしておらずgoogle認証にも登録されていないユーザは連携した上でBipokeleアカウント新規作成
+     *
+     * @param void
+     * @return redirect showRegisterOAuthUser
+     */
+    public function authGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        if(!$googleUser){
+            return redirect()->route('showLogin')->withErrors($this->googleAuthError);
+        }
+
+        // ログインしていない場合、ログインまたは登録を行いユーザのuuidを取得
+        $auth_uuid = Auth::user()->uuid ?? $this->loginUser($googleUser);
+
+        GoogleUser::firstOrCreate([
+            'google_id' => $googleUser->id,
+        ], [
+            'user_uuid' => $auth_uuid
+        ]);
+
+        return redirect()->route('showRegisterOAuthUser');
     }
 }
 

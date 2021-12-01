c9b6cda04f42b22b25dd1893b3d7d26b31119939
shm@desktop-ubuntu <s.murakami.tech@gmail.com>
2021-12-01 23:48:59 +0900

    OAuth新規登録ユーザ登録画面を実装

--- backend/resources/js/registerOAuthForm.js
+++ backend/resources/js/registerOAuthForm.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import {nameRule} from './constants/user'
 import {prefecture} from './constants/constant'
 import {getQueryParam} from './methods/method'
 import Validation from './methods/validation'
@@ -9,24 +8,66 @@ const validation = new Validation();
 new Vue({
     el: '#app',
     data: {
+        // インポートした固定値
+        prefecture_arr: prefecture,
+
+        // ユーザ情報
         uuid: '',
         name: '',
         user_profile_img_path: '',
-        prefecture_arr: '',
+        prefecture_code: 0,
+
+        // HTMLクラス属性
         nameClass: '',
-        submitStatus: false
+
+        // バリデーション
+        isNameValid: false,
     },
 
     mounted(){
-        this.prefecture_arr = prefecture;
         this.getUserDataByQueryParam();
+    },
 
-        console.log(validation.test())
+    computed: {
+        /**
+         * すべてのフォームの値が正常な場合にtrueを返す
+         *
+         * @param void
+         * @return bool
+         */
+        submitStatus: function (){
+            return (this.isNameValid && validation.isPrefecture_codeValid(this.prefecture_code));
+        }
+    },
+
+    watch: {
+        /**
+         * nameを監視しバリデーション処理を実行
+         *
+         * @param void
+         * @return bool
+         */
+        name() {
+            this.isNameValid = validation.isNameValid(this.name);
+
+            if(this.name.length === 0){
+                this.nameClass = 'form-control';
+
+            }else if(this.isNameValid){
+                this.nameClass = 'form-control is-valid';
+
+            }else{
+                this.nameClass = 'form-control is-invalid';
+            }
+        }
     },
 
     methods: {
         /**
+         * URLのクエリパラメータからユーザデータを取得し、dataに格納
          *
+         * @param void
+         * @return void
          */
         getUserDataByQueryParam: function(){
             const query = getQueryParam();
--- backend/resources/views/auth/registerOAuthForm.blade.php
+++ backend/resources/views/auth/registerOAuthForm.blade.php
@@ -17,8 +17,9 @@
 <div id="app" v-cloak>
   <form class="login-form" method="POST" action="{{ route('registerOAuthUser') }}">
     @csrf
-    <div class="form-group">
-      <img class="d-block mx-auto" alt="userProfileImg" v-bind:src="user_profile_img_path">
+    <div class="form-group mt-4">
+      <p class="mb-2">プロフィール写真</p>
+      <img class="rounded" alt="userProfileImg" v-bind:src="user_profile_img_path">
     </div>
     <div class="form-group">
       <label for="inputName">名前</label>
@@ -26,12 +27,12 @@
       <small id="nameInfo" class="form-text text-muted">公序良俗に反する内容は入力しないでください</small>
     </div>
     <div class="form-group">
-      <label for="select1">お住まいの都道府県</label>
-      <select name="prefecture_code" class="form-control" id="select1">
+      <label for="prefecture_code">お住まいの都道府県</label>
+      <select v-model="prefecture_code" name="prefecture_code" class="form-control" id="select1">
         <option value="0">選択してください</option>
         <option v-for="(prefecture, i) in prefecture_arr" v-bind:value="i+1">@{{ prefecture }}</option>
       </select>
-      <div class="text-right">
+      <div class="text-right mt-4">
         <button v-bind:disabled="!submitStatus" type="submit" class="btn btn-success">保存</button>
       </div>
     </div>
